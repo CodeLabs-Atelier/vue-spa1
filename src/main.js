29ae9fb01d35d488520d633629729ac3479b2a6e
blackBoxMod <davis.gamer07@gmail.com>
2023-07-28 15:58:50 -0400

    First Commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// ENTRY POINT FOR THE PROJECT
+
+import { createApp, VueElement } from "vue";
+import App from './components/App.vue'
 
 createApp(App).mount('#app')
